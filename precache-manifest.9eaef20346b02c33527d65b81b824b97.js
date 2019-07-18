da3451db3dbb6fa18dfb3dd6c38c8202b7034915
JIM <36786589+luoyinong@users.noreply.github.com>
2019-07-18 15:08:05 +0800

    Add files via upload

---
+++ precache-manifest.9eaef20346b02c33527d65b81b824b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b294a50c7dd42e5cf454",
+    "url": "/js/chunk-vendors.79220dca.js"
+  },
+  {
+    "revision": "a76c1dfbefba4bf4ef7b",
+    "url": "/js/app.45af629f.js"
+  },
+  {
+    "revision": "d05697103b79ec0e143e5d2dd0381541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a76c1dfbefba4bf4ef7b",
+    "url": "/css/app.578ae515.css"
+  }
+];
